700d179c0a15bef86e5bbb8d2578548930fccac3
LionelMv <mvangicode@gmail.com>
2023-09-28 23:19:41 +0300

    feat: Add pascal triangle function

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A function that returns a list of integers representing
+the Pascal's triangle of n.
+Returns an empty list if n <= 0
+"""
+from math import factorial
+
+
+def pascal_triangle(n):
+    """returns Pascal's triangle in a list of list"""
+    my_list = []
+    if (n <= 0):
+        return my_list
+    my_list.append([1])
+    for i in range(n - 1):
+        my_list.append([1] + [my_list[i][a] + my_list[i][a + 1]
+                              for a in range(len(my_list[i]) - 1)] + [1])
+    return my_list
