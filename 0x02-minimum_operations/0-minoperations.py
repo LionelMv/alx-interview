2f5edbc830388bf9d5e7a6ee4187c42aba0f6725
LionelMv <mvangicode@gmail.com>
2023-10-12 23:17:12 +0300

    fix: Change use of array to simple while loop.

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -17,16 +17,11 @@ def minOperations(n):
     int: The minimum number of operations required
         to reach n H characters. If n is impossible to achieve, return 0.
     """
-    if n <= 1:
-        return 0
-
-    # Initialize the array to store the minimum operations
-    dp = [0] * (n + 1)
-
-    for i in range(2, n + 1):
-        dp[i] = float('inf')  # Set to a large value initially
-        for j in range(1, i // 2 + 1):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
-
-    return dp[n]
+    a = 0
+    b = 2
+    while n > 1:
+        while n % b == 0:
+            a += b
+            n = n / b
+        b += 1
+    return a
