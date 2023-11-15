5f566efd4cd3fa418c46a685bfa49479733bc83b
LionelMv <mvangicode@gmail.com>
2023-11-15 10:21:27 +0300

    Remove printMovieCharacters function

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,51 +5,19 @@
  */
 
 const request = require('request');
-
-// Function to retrieve and print character names
-function printMovieCharacters (filmId) {
-  const apiUrl = `https://swapi-api.alx-tools.com/api/films/${filmId}/`;
-
-  return new Promise((resolve, reject) => {
-    request(apiUrl, (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-        reject(error || new Error(`Request failed with status code ${response.statusCode}`));
-        return;
-      }
-
-      const filmData = JSON.parse(body);
-      const characterUrls = filmData.characters;
-
-      // Use Promise.all to wait for all character requests
-      const characterPromises = characterUrls.map(characterUrl => new Promise((resolve, reject) => {
-        request(characterUrl, (error, response, body) => {
-          if (error || response.statusCode !== 200) {
-            reject(error || new Error(`Request failed with status code ${response.statusCode}`));
-            return;
-          }
-          const characterData = JSON.parse(body);
-          resolve(characterData.name);
-        });
-      }));
-
-      Promise.all(characterPromises)
-        .then(characters => resolve(characters))
-        .catch(reject);
-    });
-  });
-}
-
-// Get movie ID from command line argument
 const movieId = process.argv[2];
 
-// Call the function with the provided movie ID
-printMovieCharacters(movieId)
-  .then(characters => {
-    // Print characters in the correct order
-    characters.forEach(character => {
-      console.log(character);
+request(`https://swapi-api.alx-tools.com/api/films/${movieId}`, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const characters = JSON.parse(body).characters;
+  characters.forEach(character => {
+    request(character, function (error, response, body) {
+      if (error) {
+        console.error(error);
+      }
+      console.log(JSON.parse(body).name);
     });
-  })
-  .catch(error => {
-    console.error('An error occurred:', error.message);
   });
+});
