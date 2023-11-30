037b18bfce60da0391ab66c83db97ffea90abad2
LionelMv <mvangicode@gmail.com>
2023-11-30 10:21:28 +0300

    Add 0-island_perimeter file and its test file

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Contains a function that returns the perimeter of an island
+as described in grid.
+"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of an island.
+    """
+    if not grid or not grid[0]:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4  # Count all sides initially
+
+                # Check neighboring cells and deduct shared sides
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
