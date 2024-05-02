bbae4510997c648731e5f9a5c0f82077ea814d6a
LionelMv <mvangicode@gmail.com>
2024-05-02 16:11:10 +0300

    change from greedy algorithm to dynamic programming
    use the bottom up approach

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """
 Given a pile of coins of different values, determine the fewest number of coins
-needed to meet a given amount total
+needed to meet a given amount total.
+
+Used Dynamic programming the bottom up approach
 """
 
 
 def makeChange(coins, total):
     """this functions determines the fewest number of coins needed to meet
     a given amount total - using coins of different values"""
-    if total <= 0:
-        return 0
-    coins = sorted(coins, reverse=True)
-    curTotal = 0
-    minCoins = 0
-    for coin in coins:
-        bal = (total - curTotal)//coin
-        curTotal += bal*coin
-        minCoins += bal
-        if curTotal == total:
-            return minCoins
-    return -1
+    dp = [total + 1] * (total +1)
+    dp[0] = 0
+
+    for a in range(1, total + 1):
+        for coin in coins:
+            if a - coin >= 0:
+                dp[a] = min(dp[a], 1 + dp[a - coin])
+                # a = 7
+                # coin = 3
+                # dp[7] = 1 + dp[4]
+    return dp[total] if dp[total] != total + 1 else -1
